5670bd49b98b94f24791d32cefa2aba4472faba2
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-04-04 21:51:46 +0200

    Second Stage

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.bio, name="bio"),
+    path("photos/", views.photos, name="photos"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
     # context = {'name'}
     return render(request, "main/home.html")
+
+
+def bio(request):
+    return render(request, "main/bio.html")
+
+
+def photos(request):
+    return render(request, "main/photos.html")
